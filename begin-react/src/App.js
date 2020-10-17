672c67e412f420ace793ded4d9c5ad03d54e7d9e
Sangmin In <me@sangmin.in>
2020-10-17 14:19:48 +0900

    learn jsx

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hello from './Hello';
+import './App.css'
+
 
 function App() {
+  const name = "react"
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* this is comment */}
+      /* it's visible */
+      <Hello
+      // can write comment in tag like this
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 

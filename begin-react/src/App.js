5780bec54c98e631953ef68f594a257f67cdd6f1
Sangmin In <me@sangmin.in>
2020-10-17 16:12:03 +0900

    learn props

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
 import './App.css'
+import Wrapper from './Wrapper';
 
 
 function App() {
-  const name = "react"
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
-
   return (
-    <>
-      {/* this is comment */}
-      /* it's visible */
-      <Hello
-      // can write comment in tag like this
-      />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello color="blue" />
+      <Hello name="인상민" color="red" />
+    </Wrapper>
   );
 }
 
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-function Hello() {
-    return <div>hello!</div>
+function Hello({ color, name }) {
+    return <div style={{ color: color }}>hello! {name}</div>
+}
+
+Hello.defaultProps = {
+    name: '이름 없음'
 }
 
 export default Hello
